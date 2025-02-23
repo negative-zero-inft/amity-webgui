0cfd87173317c1c4dfa27187de5e275451f80561
nrdsstuff <nrdfoss@gmail.com>
2025-02-23 14:20:38 +0100

    github pages fuckery

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +13,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		appDir: "app",
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
 	}
 };
 
