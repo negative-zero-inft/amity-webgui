d0c0bf0b8a228c9686feb41d0544f5a87521fbb1
raluvy95 <ralucadoge069@gmail.com>
2025-02-24 11:56:52 +0200

    skibdii sigma

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +13,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: dev ? '' : ''
+			base: ''
 		}
 	}
 };
