ee453c89b3d16a9a36bf825afe66c0625ef861d7
meowstercatel <66054649+meowstercatel@users.noreply.github.com>
2025-02-26 10:57:38 +0100

    oauth backend integration

---
+++ src/routes/oauth/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	const token = page.url.searchParams.get('token') ?? "";
+	const server = page.url.searchParams.get('server') ?? "";
+
+	localStorage.setItem('token', token);
+	localStorage.setItem('server', server);
+	window.location.replace('/chat');
+</script>
