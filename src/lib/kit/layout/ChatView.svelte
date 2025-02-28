1cbe323a2a59bc53ee87f8c92fc1f13e3e3f46fd
raluvy95 <ralucadoge069@gmail.com>
2025-02-28 22:02:01 +0200

    refactor

---
+++ src/lib/kit/layout/ChatView.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import Avatar from '$lib/kit/Avatar.svelte';
+	import Message from '$lib/kit/Message.svelte';
+	import Cluster from './Cluster.svelte';
+</script>
+
+<div class="chatView">
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+	<Cluster
+		isForeign={true}
+		messages={['WHAT DA WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'skibidi toilet']}
+	/>
+	<Cluster messages={['skibidi toilet', 'moyay']} />
+</div>
+
+<style lang="scss">
+	@use '$lib/style/colors.scss' as c;
+	@use '$lib/style/variables.scss' as v;
+
+	.chatView {
+		width: 100%;
+		overflow-y: scroll;
+		display: flex;
+		flex-direction: column-reverse;
+		padding: v.$spacing-def;
+		gap: v.$spacing-def;
+		box-sizing: border-box;
+		padding-top: 10px;
+		height: 100%;
+	}
+</style>
