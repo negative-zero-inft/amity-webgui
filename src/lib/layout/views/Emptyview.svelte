b109d500101142c5bc72865d04eff7222fdccd19
nrdsstuff <nrdfoss@gmail.com>
2025-03-03 22:36:02 +0100

    nicer empty view

--- src/lib/layout/views/Emptyview.svelte
+++ src/lib/layout/views/Emptyview.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import { _ } from "svelte-i18n";
+    import amy from "$lib/amy.svg"
+	import Icon from "$lib/kit/decor/Icon.svelte";
 </script>
 
 <div class="emptyView">
+	<img alt="Amy" class="amy" src={amy}/>
+    <Icon size={32} name="Chat"/>
     {$_("chat.emptyView")}
 </div>
 
@@ -12,10 +16,21 @@
     @use "$lib/style/variables.scss" as v;
 
     .emptyView{
+		background: radial-gradient(#300000, #000000);
         width: 100%;
         height: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
+        font-size: 24px;
+        gap: 20px;
     }
+
+    .amy{
+		position:fixed;
+		width: 780px;
+		opacity: 0.25;
+		right: -200px;
+		bottom: -200px;
+	}
 </style>
