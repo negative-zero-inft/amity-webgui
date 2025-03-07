b72231a233230dcb799391933461926ed4f4b35c
raluvy95 <ralucadoge069@gmail.com>
2025-03-07 17:11:29 +0200

    Added force sign out in case of stuck loading (dev only)

--- src/lib/layout/devTools/DevWindow.svelte
+++ src/lib/layout/devTools/DevWindow.svelte
@@ -1,65 +1,108 @@
 <script lang="ts">
-	import Label from "$lib/kit/decor/Label.svelte";
-	import Textbox from "$lib/kit/text/Textbox.svelte";
-	import { currentFolder, folderClickEvent, ctxFolder, folderCtxMenuView } from "$lib/scripts/chatViews";
-    import { locale } from "$lib/scripts/globalData";
-    import { locale as ilocale, locales } from "svelte-i18n"; 
-    import Icon from "$lib/kit/decor/Icon.svelte";
-	import Button from "$lib/kit/gizmos/Button.svelte";
-    import { user } from "$lib/scripts/globalData";
-    
-    let listLanguages = $state($locales);
+	import Label from '$lib/kit/decor/Label.svelte';
+	import Textbox from '$lib/kit/text/Textbox.svelte';
+	import {
+		currentFolder,
+		folderClickEvent,
+		ctxFolder,
+		folderCtxMenuView
+	} from '$lib/scripts/chatViews';
+	import { locale } from '$lib/scripts/globalData';
+	import { locale as ilocale, locales } from 'svelte-i18n';
+	import Icon from '$lib/kit/decor/Icon.svelte';
+	import Button from '$lib/kit/gizmos/Button.svelte';
+	import { user } from '$lib/scripts/globalData';
 
-    function onclick(e: any) {
-        locale.set(e.target.value)
-        ilocale.set(e.target.value)
-    }
+	let listLanguages = $state($locales);
 
-    let isVisible = $state(true)
+	function onclick(e: any) {
+		locale.set(e.target.value);
+		ilocale.set(e.target.value);
+	}
+
+	let isVisible = $state(true);
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div 
-    class="window"
-    style="display: {isVisible ? 'flex' : 'none'}"
-    oncontextmenu={(e)=>{
-        e.preventDefault()
-        isVisible = false
-    }}
+<div
+	class="window"
+	style="display: {isVisible ? 'flex' : 'none'}"
+	oncontextmenu={(e) => {
+		e.preventDefault();
+		isVisible = false;
+	}}
 >
-    <code>LE EPIC DEV WINDOW</code>
-    <Label icon="Translate" label="Lingo switcher">
-        <select name="cars" {onclick}>
-            {#each listLanguages as lang}
-                <option value={lang}>{lang}</option>
-            {/each}
-          </select>
-    </Label>
-    <Label icon="Code" label="debug thingies">
-        <div class="elem-horiz">current folder: <Icon name={$currentFolder.icon}/> {$currentFolder.name}</div>
-        <Button width="100%" action={()=>{console.log($user)}} scaleClick={0.95} scaleHover={1.05}>
-            <Icon name="User"/>
-            view user
-        </Button>
-        <Button width="100%" action={()=>{console.log($folderClickEvent)}} scaleClick={0.95} scaleHover={1.05}>
-            <Icon name="Folder/Default"/>
-            view folder click event
-        </Button>
-        <Button width="100%" action={()=>{console.log($ctxFolder, $folderCtxMenuView, $ctxFolder)}} scaleClick={0.95} scaleHover={1.05}>
-            <Icon name="Folder/Default"/>
-            view folder ctx menu
-        </Button>
-    </Label>
+	<code>LE EPIC DEV WINDOW</code>
+	<Label icon="Translate" label="Lingo switcher">
+		<select name="cars" {onclick}>
+			{#each listLanguages as lang}
+				<option value={lang}>{lang}</option>
+			{/each}
+		</select>
+	</Label>
+	<Label icon="Code" label="debug thingies">
+		<div class="elem-horiz">
+			current folder: <Icon name={$currentFolder.icon} />
+			{$currentFolder.name}
+		</div>
+		<Button
+			width="100%"
+			action={() => {
+				console.log($user);
+			}}
+			scaleClick={0.95}
+			scaleHover={1.05}
+		>
+			<Icon name="User" />
+			view user
+		</Button>
+		<Button
+			width="100%"
+			action={() => {
+				console.log($folderClickEvent);
+			}}
+			scaleClick={0.95}
+			scaleHover={1.05}
+		>
+			<Icon name="Folder/Default" />
+			view folder click event
+		</Button>
+		<Button
+			width="100%"
+			action={() => {
+				console.log($ctxFolder, $folderCtxMenuView, $ctxFolder);
+			}}
+			scaleClick={0.95}
+			scaleHover={1.05}
+		>
+			<Icon name="Folder/Default" />
+			view folder ctx menu
+		</Button>
+	</Label>
+	<Label icon="Hammer" label="Action">
+		<Button
+			width="100%"
+			action={() => {
+				localStorage.removeItem('tokens');
+				window.location.replace('/login');
+			}}
+			scaleClick={0.95}
+			scaleHover={1.05}
+		>
+			<Icon name="Exit" />
+			Force sign out
+		</Button>
+	</Label>
 </div>
 
 <style lang="scss">
-    @use '$lib/style/variables.scss' as v;
-    @use '$lib/style/colors.scss' as c;
+	@use '$lib/style/variables.scss' as v;
+	@use '$lib/style/colors.scss' as c;
 
-    .window{
-        gap: v.$spacing-def;
-        position: absolute;
-        bottom: 10px;
-        right: 10px;
-    }
+	.window {
+		gap: v.$spacing-def;
+		position: absolute;
+		bottom: 10px;
+		right: 10px;
+	}
 </style>
